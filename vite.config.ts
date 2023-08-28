7893a825dc329270551e7e6e23c99b0475f39a9f
YK <130263480+HARUHARU0220@users.noreply.github.com>
2023-08-28 16:59:06 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173  
-  }, // port  
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
