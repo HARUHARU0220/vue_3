f388b13ec46905a2905b4b9c339d2f87102fb4fc
YK <130263480+HARUHARU0220@users.noreply.github.com>
2023-09-03 23:28:41 +0900

    Add files via upload

---
+++ HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!---create a from that allows users to add a restaurant to a list-->
+    <form @submit.prevent = "addRestaurant">
+      <label for = "restaurant-name"> Restaurant Name </label>
+      <input id = "restaurant-name" v-model = "restaurantName"
+             type = "text" />
+      <button type = "submit">Add Restaurant</button>
+    </form>
+  </main>
+</template>
