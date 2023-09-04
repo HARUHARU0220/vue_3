766fc0ec636c0a644ec8896d5d629f3e93f69ac6
YK <130263480+HARUHARU0220@users.noreply.github.com>
2023-09-04 22:03:52 +0900

    feature: add Homeview.vue

--- HomeView.vue
+++ HomeView.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+import { ref } from 'vue'
+
+const restaurantList = ref([])
+const newRestaurantName = ref('')
+
+function addRestaurant() {
+  restaurantList.value.push(newRestaurantName.value)
+
+}
+
 </script>
 
 <template>
@@ -11,5 +21,10 @@ import TheWelcome from '../components/TheWelcome.vue'
              type = "text" />
       <button type = "submit">Add Restaurant</button>
     </form>
+    <ul>
+      <li v-for ="restaurant in restaurantList" key="restaurant">
+        {{ restaurant }}
+      </li>
+    </ul>
   </main>
 </template>
